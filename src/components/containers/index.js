c6d6ec9bf818c2e68483d3dcd39a1230eca5140e
YosephAhmed <yosephahmed@aol.com>
2020-06-25 11:59:46 -0400

    Refactored Home component to a NavbarContainer and NavbarView. Then, we connected the NavbarContainer to the store and obtained a reference that shows if the user is logged in or not. This is then passed as props to the NavbarView, where it will display navbar information based on its state.

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,2 +1,3 @@
 export { default as HomeContainer } from "./HomeContainer";
 export { default as SignUpContainer } from "./SignUpContainer";
+export { default as NavbarContainer } from "./NavbarContainer";
