5e59b1b1fe3c7011b7a16e3dc06c00cc297b7780
YosephAhmed <yosephahmed@aol.com>
2020-06-25 14:38:57 -0400

    Added ProfileContainer and ProfileView for the profile route. Added the route to the RoutesView, where path is /profile. Sanity check on ProfileView to ensure that ProfileComponent is rendering it correctly.

---
+++ src/components/containers/ProfileContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { ProfileView } from "../views";
+
+export default class ProfileContainer extends Component {
+    render() {
+        return (
+            <div>
+                <ProfileView />
+            </div>
+        )
+    }
+}
---
+++ src/components/views/ProfileView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProfileView(props) {
+    return (
+        <div>
+            Hello from profile view
+        </div>
+    )
+}
