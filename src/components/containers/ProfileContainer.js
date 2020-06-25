1c863241ddd12017b839c692a050c888ba805712
YosephAhmed <yosephahmed@aol.com>
2020-06-25 14:51:00 -0400

    Connected ProfileContainer to the Redux Store. mapDispatch will populate userInfo with information obtained by me thunk. In ProfileView, it takes the userInfo as a prop then atempts to display the username from that object.

--- src/components/containers/ProfileContainer.js
+++ src/components/containers/ProfileContainer.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { ProfileView } from "../views";
+import { connect } from "react-redux";
+import { me } from "../../thunks";
 
-export default class ProfileContainer extends Component {
-    render() {
-        return (
-            <div>
-                <ProfileView />
-            </div>
-        )
-    }
+class ProfileContainer extends Component {
+  render() {
+    return (
+      <div>
+        <ProfileView userInfo={this.props.userInfo} />
+      </div>
+    );
+  }
 }
+
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.user.id,
+    userInfo: state.userInfo,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+      //userInfo should be an object with all the user info
+    userInfo: () => dispatch(me()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(ProfileContainer);
--- src/components/views/ProfileView.js
+++ src/components/views/ProfileView.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function ProfileView(props) {
-    return (
-        <div>
-            Hello from profile view
-        </div>
-    )
+  return (
+    <div>
+      <h1>Username: {props.userInfo.username}</h1>
+    </div>
+  );
 }
