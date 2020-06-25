776ecf3164a983204a8754da48928a6a997abf38
Andrea Habib <andrea.atef@yahoo.com>
2020-06-25 15:37:22 -0400

    updated backend

--- backend/database/models/index.js
+++ backend/database/models/index.js
@@ -1,14 +1,19 @@
 const User = require("./user");
 const Zipcode = require("./zipcode")
-const Symptoms = require("./symptoms")
+const Symptoms = require("./symptoms");
+const { isCompositeComponentWithType } = require("react-dom/test-utils");
 
 // Make associations here, if necessary;
+Zipcode.hasMany(User, { foreignKey: 'username', sourceKey: 'user' })
+User.belongsTo(Zipcode, { foreignKey: 'username', targetKey: 'user' })
 
-// zipcode has a field called userID but user does not have any zipcodes
 
-// Information about association is present in the target model, foregin key constraint of zipcode
-Zipcode.hasMany(User)
-User.hasOne(Symptoms)
+
+
+
+// Information about association is present in the target model, foregin key constraint of zipcode, select zipcode from users;
+// Zipcode.hasMany(User, { as: 'User' })
+// User.hasOne(Symptoms)
 
 module.exports = {
   User,
